e623889a89ee3e5fc87ebb2976a88730715648d2
Yogesh <yogeshyadav.w@gmail.com>
2022-12-08 17:04:33 +0530

    second commit

--- sici.py
+++ sici.py
@@ -1,11 +1,13 @@
-principal = float(input('Enter amount: '))
-time = float(input('Enter time: '))
-rate = float(input('Enter rate: '))
+# principal = float(input('Enter amount: '))
+# time = float(input('Enter time: '))
+# rate = float(input('Enter rate: '))
 
-# Calcualtion
-simple_interest = (principal*time*rate)/100
-compound_interest = principal * ( (1+rate/100)**time - 1)
+# # Calcualtion
+# simple_interest = (principal*time*rate)/100
+# compound_interest = principal * ( (1+rate/100)**time - 1)
 
-# Displaying result
-print('Simple interest is: %f' % (simple_interest))
-print('Compound interest is: %f' %(compound_interest))
+# # Displaying result
+# print('Simple interest is: %f' % (simple_interest))
+# print('Compound interest is: %f' %(compound_interest))
+
+print("hello!!!1")
